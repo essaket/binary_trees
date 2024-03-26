b2491ebf98283d3a8fad31849f8b2999ff5fabd4
essaket <hichamessaket17gmail.com>
2024-03-26 11:20:15 -0700

    Writing task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,3 +15,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to measure the height
+ *
+ * Return: height of a a binary tree or 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t size_left = 0, size_right = 0;
+
+		size_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((size_left > size_right) ? size_left : size_right);
+	}
+
+	return (0);
+}
