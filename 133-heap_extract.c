308279df3b2308e3fceb66721fba8371a392fd84
essaket <hichamessaket17gmail.com>
2024-03-28 13:12:21 -0700

    Writing task 39

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -40,9 +40,11 @@ int heap_extract(heap_t **root)
 
 void recurse_extract(heap_t *tree)
 {
+	void recurse_extract(heap_t *tree)
+{
 	heap_t *sub_max, *right_max = NULL;
 
-	if (!(tree->left))
+	if (!tree->left)
 		return;
 	sub_max = max((tree)->left);
 	if (tree->right)
@@ -50,7 +52,7 @@ void recurse_extract(heap_t *tree)
 	if (right_max && right_max->n > sub_max->n)
 		sub_max = right_max;
 	tree->n = sub_max->n;
-	if (!(sub_max->left))
+	if (!sub_max->left)
 	{
 		if (sub_max->parent && sub_max->parent->left == sub_max)
 			sub_max->parent->left = NULL;
@@ -65,15 +67,15 @@ void recurse_extract(heap_t *tree)
 /**
  * max - Finds the maximum node in a tree.
  *
- * @tree: the pointer to the root of the tree.
+ * @tree: The pointer to the root of the tree.
  *
- * Return: the node with the maximum value.
+ * Return: The node with the maximum value.
  */
 heap_t *max(heap_t *tree)
 {
 	heap_t *curr_max, *left_max, *right_max;
 
-	if (!(tree->left))
+	if (!tree->left)
 		return (tree);
 	left_max = max(tree->left);
 	if (left_max->n > tree->n)
