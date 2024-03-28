2123ce6e230d1dc569eeae5e9f2a2d4cd0e4e5c8
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-03-28 20:08:20 +0000

    Writing task 39

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 
+void recurse_extract(heap_t *tree);
+heap_t *max(heap_t *tree);
+
 /**
  * heap_extract - Extracts the root node of a Max Binary Heap
  *
@@ -39,7 +42,7 @@ void recurse_extract(heap_t *tree)
 {
 	heap_t *sub_max, *right_max = NULL;
 
-	if (!tree->left)
+	if (!(tree->left))
 		return;
 	sub_max = max((tree)->left);
 	if (tree->right)
@@ -47,7 +50,7 @@ void recurse_extract(heap_t *tree)
 	if (right_max && right_max->n > sub_max->n)
 		sub_max = right_max;
 	tree->n = sub_max->n;
-	if (!sub_max->left)
+	if (!(sub_max->left))
 	{
 		if (sub_max->parent && sub_max->parent->left == sub_max)
 			sub_max->parent->left = NULL;
@@ -62,15 +65,15 @@ void recurse_extract(heap_t *tree)
 /**
  * max - Finds the maximum node in a tree.
  *
- * @tree: The pointer to the root of the tree.
+ * @tree: the pointer to the root of the tree.
  *
- * Return: The node with the maximum value.
+ * Return: the node with the maximum value.
  */
 heap_t *max(heap_t *tree)
 {
 	heap_t *curr_max, *left_max, *right_max;
 
-	if (!tree->left)
+	if (!(tree->left))
 		return (tree);
 	left_max = max(tree->left);
 	if (left_max->n > tree->n)
