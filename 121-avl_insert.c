50ebfff4b3cf94d5703d758c588053692fe5f89f
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-03-28 16:47:58 +0000

    Writing task 31

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -42,38 +42,37 @@ avl_t *avl_insert(avl_t **tree, int value)
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new_avl, int value)
 {
-	int x;
+	int bfactor;
 
-	if (!(*tree))
-		return (*new_avl = binary_tree_node(parent, value));
+	if (*tree == NULL)
+		return (*new = binary_tree_node(parent, value));
 
 	if ((*tree)->n > value)
 	{
-		(*tree)->left = avl_insert_recursive(&(*tree)->left, *tree, new_avl, value);
-		if (!((*tree)->left))
+		(*tree)->left = avl_insert_recursive(&(*tree)->left, *tree, new, value);
+		if ((*tree)->left == NULL)
 			return (NULL);
 	}
 	else if ((*tree)->n < value)
 	{
-		(*tree)->right = avl_insert_recursive(&(*tree)->right,
-				*tree, new_avl, value);
-		if (!((*tree)->right))
+		(*tree)->right = avl_insert_recursive(&(*tree)->right, *tree, new, value);
+		if ((*tree)->right == NULL)
 			return (NULL);
 	}
 	else
 		return (*tree);
 
-	x = balance(*tree);
-	if (x > 1 && (*tree)->left->n > value)
+	bfactor = balance(*tree);
+	if (bfactor > 1 && (*tree)->left->n > value)
 		*tree = binary_tree_rotate_right(*tree);
-	else if (x < -1 && (*tree)->right->n < value)
+	else if (bfactor < -1 && (*tree)->right->n < value)
 		*tree = binary_tree_rotate_left(*tree);
-	else if (x > 1 && (*tree)->left->n < value)
+	else if (bfactor > 1 && (*tree)->left->n < value)
 	{
 		(*tree)->left = binary_tree_rotate_left((*tree)->left);
 		*tree = binary_tree_rotate_right(*tree);
 	}
-	else if (x < -1 && (*tree)->right->n > value)
+	else if (bfactor < -1 && (*tree)->right->n > value)
 	{
 		(*tree)->right = binary_tree_rotate_right((*tree)->right);
 		*tree = binary_tree_rotate_left(*tree);
