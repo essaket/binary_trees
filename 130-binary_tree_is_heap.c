3ebd5f1c67b914aa86b391aa26f2a438b8ae7728
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-03-28 18:29:54 +0000

    Writing task 36

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+int check_parent(const binary_tree_t *tree);
+int binary_tree_is_complete(const binary_tree_t *tree);
+int tree_is_complete(const binary_tree_t *tree, int i, int cnodes);
+size_t binary_tree_size(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_heap - Checks if a binary tree is a valid Max Binary Heap
  *
@@ -17,13 +22,14 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 /**
  * check_parent - Checks if parent has a greater value than its childs.
  *
- * @tree: A pointer to the node.
+ * @tree: a pointer to the node.
  *
  * Return: 1 if parent has a greater value, 0 otherwise
  */
+
 int check_parent(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
 
 	if (tree->n > tree->parent->n)
@@ -35,14 +41,16 @@ int check_parent(const binary_tree_t *tree)
 /**
  * binary_tree_is_complete - Calls to tree_is_complete function.
  *
- * @tree: Tree root.
+ * @tree: tree root.
+ *
  * Return: 1 if tree is complete, 0 otherwise
  */
+
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	size_t cnodes;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
 	cnodes = binary_tree_size(tree);
@@ -53,15 +61,16 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 /**
  * tree_is_complete - Checks if tree is complete.
  *
- * @tree: Pointer to the tree root.
- * @i: Node index.
- * @cnodes: Number of nodes.
+ * @tree: pointer to the tree root.
+ * @i: node index.
+ * @cnodes: number of nodes.
  *
  * Return: 1 if tree is complete, 0 otherwise
  */
+
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (1);
 
 	if (i >= cnodes)
@@ -75,13 +84,12 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 /**
  * binary_tree_size - Measures the size of a binary tree.
  *
- * @tree: Tree root.
- * Return: Size of the tree or 0 if tree is NULL.
+ * @tree: tree root.
+ *
+ * Return: size of the tree or 0 if tree is NULL.
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (!tree ? 0 : binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
